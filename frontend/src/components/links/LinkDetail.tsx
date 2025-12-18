5fedd37bbff562d99315593eb851f4f3c4f0e2b1
ibecir <becir@bioptimizers.com>
2025-12-18 21:51:43 +0100

    Commit

--- frontend/src/components/links/LinkDetail.tsx
+++ frontend/src/components/links/LinkDetail.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import {
   ArrowLeft,
   ExternalLink,
